a138906a4aa4f95d19af342120173483087dca6e
torresmarcoa <marcotorresaceves@gmail.com>
2024-10-04 20:47:34 -0600

    form

---
+++ form/js/scripts.js
@@ -0,0 +1,23 @@
+const currentUrl = window.location.href;
+
+const everthing = currentUrl.split("?");
+
+let formData = everthing[1].split("&");
+
+function show(cup) {
+    let result;
+    formData.forEach((element) => {
+        if (element.startsWith(cup)) {
+            result = element.split("=")[1].replace("%40", "@").replace("+", " ").replace("%2B", "+");
+        }
+        
+    });
+    return (result);
+}
+
+const showInfo = document.querySelector("#results");
+showInfo.innerHTML =  ` 
+<p>Appointment for ${show("firs")} ${show("last")}</p>
+<p>Proxy ${show("ordinance")} on ${show("fecha")} in the ${show("location")} Temple. </p>
+<p>Your Phone: ${show("phone")}</p>
+<p>Your E-mail: <a href="${show("email")} target="_blank"">${show("email")}</a></p>`
