6ff9002734b8365cbb1999eed14abdf97bec2ad3
torresmarcoa <marcotorresaceves@gmail.com>
2024-10-18 22:36:14 -0600

    update json

--- project/scripts/agencies.js
+++ project/scripts/agencies.js
@@ -1,6 +1,6 @@
 import { displayAgencies } from "./displayAgencies.js";
 
-const url = `https://ll.thespacedevs.com/2.2.0/agencies/?limit=15&ordering=-total_launch_count&mode=detailed&format=json`;
+const url = `https://ll.thespacedevs.com/2.2.0/agencies/?limit=15&ordering=-total_launch_count&mode=listd&format=json`;
 
 const agenciesmain = document.getElementById("agenciesgrid");
 
--- project/scripts/modal.js
+++ project/scripts/modal.js
@@ -1,12 +1,10 @@
 export const displayAgencyDetail = (agency) => {
     const agenciesModal = document.querySelector("dialog");
-    console.log(agenciesModal);
-
+    
     agenciesModal.innerHTML = `
         <button id="closeModal">X</button>
         <h2>${agency.name}</h2>
         <h3>Country: ${agency.country_code}</h3>
-        <p>Total Launch Count: ${agency.total_launch_count}</p>
         <p>${agency.description}</p>
     `;
 
