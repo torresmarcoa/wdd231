d054781f2eb3ac6e406292cc3f241d9fd7f272dd
torresmarcoa <marcotorresaceves@gmail.com>
2024-10-23 20:47:31 -0600

    html fix and console log deleted

--- project/scripts/agencies.js
+++ project/scripts/agencies.js
@@ -16,7 +16,6 @@ async function agenciesjson() {
         if (response.ok) {
             const data = await response.json();
             agenciesData = data.results
-            console.log(agenciesData)
             displayAgencies(agenciesData, agenciesmain);
         } else {
             throw new Error(await response.text());
